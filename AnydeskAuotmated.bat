a92e90efa74335ac2c6150ff9d20fb1582709adb
Justin Fairlie <104134455+Justin-Fairlie@users.noreply.github.com>
2024-10-31 14:37:51 +0200

    Add files via upload

---
+++ AnydeskAuotmated.bat
@@ -0,0 +1,17 @@
+:loop
+
+echo Bp*Xoy$i32 | "C:/Program Files/AnyDesk/anydesk.exe" 1420240684 --with-password
+timeout /t 10
+
+
+if %errorlevel% neq 0 (
+    echo Connection lost. Powering on remote device...
+    "C:/Program Files/AnyDesk/anydesk.exe" %anydesk_id% --remote-restart
+    timeout /t 10
+    echo Remote device powered on. Reconnecting...
+    goto loop
+) else (
+    echo Connected. Waiting for 10 seconds...
+    timeout /t 10
+    goto loop
+)
